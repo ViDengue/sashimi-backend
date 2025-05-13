63a834f1069e1e8245ec6a2d05855dac851c31e8
phucdoan2003 <thienphucdoantran@gmail.com>
2025-05-13 23:48:29 +0700

    feat: create embeddings + semantic search

--- main.go
+++ main.go
@@ -8,21 +8,20 @@ import (
 	"os"
 
 	chroma "github.com/amikos-tech/chroma-go/pkg/api/v2"
-	"github.com/amikos-tech/chroma-go/pkg/embeddings"
-	defaultef "github.com/amikos-tech/chroma-go/pkg/embeddings/default_ef"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
 var ChromaClient chroma.Client
-var embedderEndpoint = "http://localhost:5005/embed"
 
 func main() {
 
+	ctx := context.Background()
+
 	chromaEndpoint, ok := os.LookupEnv("CHROMA_ENDPOINT")
 	if !ok {
-		chromaEndpoint = "localhost:8000"
+		chromaEndpoint = "http://localhost:8000"
 	}
 
 	ChromaClient, err := chroma.NewHTTPClient(
@@ -34,12 +33,64 @@ func main() {
 	}
 
 	fmt.Printf("Successfully connect to ChromaDB: %s\n", ChromaClient)
+
+	collection, err := ChromaClient.GetOrCreateCollection(ctx, "test")
+	if err != nil {
+		log.Fatalf("Failed to create collection: %v", err)
+	}
+
 	app := pocketbase.New()
 
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
 		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
 
-		se.Router.POST("/api/embed", CreateTextAsEmbedding)
+		se.Router.POST("/api/embed", func(e *core.RequestEvent) error {
+
+			type CreateTextAsEmbeddingRequest struct {
+				Text string `json:"text"`
+			}
+			data := CreateTextAsEmbeddingRequest{}
+
+			if err := e.BindBody(&data); err != nil {
+				return e.BadRequestError("Failed to read request data", err)
+			}
+
+			err := collection.Add(
+				context.Background(),
+				chroma.WithIDGenerator(chroma.NewULIDGenerator()),
+				chroma.WithTexts(data.Text),
+			)
+			if err != nil {
+				return e.InternalServerError("Failed to create embedding. Cringe", err)
+			}
+
+			return e.JSON(http.StatusOK, map[string]any{
+				"message": "Saved to ChromaDB",
+			})
+		},
+		)
+
+		se.Router.POST("/api/similar", func(e *core.RequestEvent) error {
+			type SimilarSearchRequest struct {
+				Text  string `json:"text"`
+				Limit int    `json:"limit"`
+			}
+			data := SimilarSearchRequest{}
+
+			if err := e.BindBody(&data); err != nil {
+				return e.BadRequestError("Failed to read request data", err)
+			}
+			qr, err := collection.Query(context.Background(), chroma.WithQueryTexts(data.Text), chroma.WithNResults(data.Limit))
+			if err != nil {
+				return e.InternalServerError("Error querying collection: %s\n", err)
+			}
+			fmt.Println(qr.GetDocumentsGroups())
+
+			return e.JSON(http.StatusOK, map[string]any{
+				"similar_content": qr.GetDocumentsGroups(),
+			})
+
+		})
 
 		return se.Next()
 	})
@@ -54,52 +105,5 @@ func main() {
 			log.Fatalf("Error closing ChromaDB Client: %s\n", err)
 		}
 	}()
-}
-
-type CreateTextAsEmbeddingRequest struct {
-	text string `json:"text"`
-}
-
-func CreateTextAsEmbedding(e *core.RequestEvent) error {
-	data := CreateTextAsEmbeddingRequest{}
-
-	if err := e.BindBody(&data); err != nil {
-		return e.BadRequestError("Failed to read request data", err)
-	}
-
-	resp, err := CreateEmbeddingFromText(data.text)
-	if err != nil {
-		return e.InternalServerError("Failed to create embedding. Cringe", err)
-	}
-
-	return e.JSON(http.StatusOK, map[string]any{
-		"message":   "Saved to ChromaDB",
-		"embedding": resp,
-	})
-}
-
-func GetSimilarText(e *core.RequestEvent) error {
-	return e.JSON(http.StatusOK, map[string]any{})
-}
-
-func CreateEmbeddingFromText(text string) ([]embeddings.Embedding, error) {
-	ef, closeef, efErr := defaultef.NewDefaultEmbeddingFunction()
-
-	defer func() {
-		err := closeef()
-		if err != nil {
-			fmt.Printf("Error closing default embedding function: %s \n", err)
-		}
-	}()
-	if efErr != nil {
-		fmt.Printf("Error creating OpenAI embedding function: %s \n", efErr)
-	}
-
-	resp, reqErr := ef.EmbedDocuments(context.Background(), []string{text})
-	if reqErr != nil {
-		fmt.Printf("Error embedding documents: %s \n", reqErr)
-	}
-
-	return resp, nil
 
 }
